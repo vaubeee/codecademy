a6dc482617f3afda2ad6f54bdc329f3e7f1a0ef3
vaubeee <92851534+vaubeee@users.noreply.github.com>
2021-12-22 22:35:53 +0100

    constructor

--- kladde.py
+++ kladde.py
@@ -675,7 +675,7 @@ isabelles_ices = Store()
 alternative_rocks.store_name = "Alternative Rocks"
 
 isabelles_ices.store_name = "Isabelle's Ices"
-'''
+
 can_we_count_it = [{'s': False}, "sassafrass", 18, ["a", "c", "s", "d", "s"]]
 for element in can_we_count_it:
   
@@ -684,4 +684,26 @@ for element in can_we_count_it:
     
   else:
     print(str(type(element)) + " does not have the count attribute :(")
-  
+'''
+
+#Constructors
+class Circle:
+  pi = 3.14
+  def __init__(self, diameter):
+    print("Creating circle with diameter {d}".format(d=diameter))
+    # Add assignment for self.radius here:
+    self.radius = diameter / 2
+
+  def circumference(self):
+    circumference = 2 * self.pi * self.radius
+    return circumference
+
+medium_pizza = Circle(12)
+teaching_table = Circle(36)
+round_room = Circle(11460)
+
+print(medium_pizza.circumference())
+print(teaching_table.circumference())
+print(round_room.circumference())
+
+
