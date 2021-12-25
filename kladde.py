c84e0f02f119d111cafd51d561d51b22c99ea8ea
vaubeee <92851534+vaubeee@users.noreply.github.com>
2021-12-25 22:05:04 +0100

    class students not ready yet

--- kladde.py
+++ kladde.py
@@ -685,7 +685,6 @@ for element in can_we_count_it:
   else:
     print(str(type(element)) + " does not have the count attribute :(")
 
-'''
 #Constructors
 class Circle:
   pi = 3.14
@@ -713,7 +712,6 @@ print(medium_pizza) #prints out def __repr__
 print(teaching_table)
 print(round_room)
 
-'''
 #dir
 print(dir(5))
 print("")
@@ -722,4 +720,51 @@ def this_function_is_an_object():
   return
 
 print(dir(this_function_is_an_object))
+'''
+class Student:
+  def __init__(self, name, year):
+    self.name = name
+    self.year = year
+    self.grades = []
+  
+  def add_grade(self, grade):
+    if type(grade) is Grade:
+      self.grades.append(grade)
+ 
+  def print_grades(self):
+    for grade in self.grades:
+      print(f"{self.name}'s grade is {grade.score}")
+    return 1
+
+class Grade:
+  minimum_passing = 65
+  
+  def __init__(self, score):
+    self.score = score
+    
+roger = Student("Roger van der Weyden", 10)
+sandro = Student("Sandro Botticelli", 12)
+pieter = Student("Pieter Bruegel the Elder", 8)
+
+pieter.add_grade(Grade(100))
+pieter.add_grade(Grade(80))
+pieter.add_grade(Grade(55))
+roger.add_grade(Grade(60))
+roger.add_grade(Grade(80))
+roger.add_grade(Grade(95))
+
+pieter.print_grades()
+roger.print_grades()
+
+'''
+Great job! You’ve created two classes and defined their interactions. This is object-oriented programming! From here you could:
+
+Write a Grade method .is_passing() that returns whether a Grade 
+has a passing .score.
+Write a Student method get_average() that returns the student’s 
+average score.
+Add an instance variable to Student that is a dictionary 
+called .attendance, with dates as keys and booleans as values 
+that indicate whether the student attended school that day.
+Write your own classes to do whatever logic you want!
 '''
